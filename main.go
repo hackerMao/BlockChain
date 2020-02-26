221563d60751c2ef381d365534ba33cf9788cbe8
hackerMao <hacker_murray@163.com>
2020-02-26 20:49:41 +0800

    modify main.go-->create block chain and genesis block!

--- main.go
+++ main.go
@@ -31,10 +31,29 @@ func (self *Block) SetHash() {
 	self.Hash = hash[:]
 }
 
+type BlockChain struct {
+	// 区块链数组
+	blocks []*Block
+}
+
+func NewBlockChain() *BlockChain {
+	genesisBlock := GenesisBlock() 
+	return  &BlockChain{
+		blocks:[]*Block{genesisBlock},
+	}
+}
+
+func GenesisBlock() *Block {
+	return NewBlock([]byte{}, "这是区块链的第一个区块！")
+}
+
 func main() {
-	block := NewBlock([]byte{}, "可不可以给我一枚比特币？")
-	fmt.Printf("前区块哈希值：%x\n", block.PrevHash)
-	fmt.Printf("当前区块哈希值：%x\n", block.Hash)
-	fmt.Printf("数据：%s\n", block.Data)
-	//fmt.Println(len("0935d481a8c7db864698443edcf84268f784a93cba5c63b7587e7c6cf60668c7"))
+	bc := NewBlockChain()
+	for index, block := range bc.blocks {
+		fmt.Printf("======================当前区块高度：%d  ======================\n", index+1)
+		fmt.Printf("前区块哈希值：%x\n", block.PrevHash)
+		fmt.Printf("当前区块哈希值：%x\n", block.Hash)
+		fmt.Printf("数据：%s\n", block.Data)
+	}
+	fmt.Println("======================区块结束======================")
 }
