542d0f01330f4830a3476f8397307e7f00c8e23d
hackerMao <hacker_murray@163.com>
2020-02-27 10:12:02 +0800

    添加区块到区块链

--- main.go
+++ main.go
@@ -37,9 +37,9 @@ type BlockChain struct {
 }
 
 func NewBlockChain() *BlockChain {
-	genesisBlock := GenesisBlock() 
-	return  &BlockChain{
-		blocks:[]*Block{genesisBlock},
+	genesisBlock := GenesisBlock()
+	return &BlockChain{
+		blocks: []*Block{genesisBlock},
 	}
 }
 
@@ -47,8 +47,19 @@ func GenesisBlock() *Block {
 	return NewBlock([]byte{}, "这是区块链的第一个区块！")
 }
 
+func (self *BlockChain) AddBlock(data string) {
+	// 获取区块链中最后一个区块，取该区块的哈希值作为上一个哈希值
+	lastlock := self.blocks[len(self.blocks)-1]
+	// 新建一个区块
+	block := NewBlock(lastlock.Hash, data)
+	// 添加区块
+	self.blocks = append(self.blocks, block)
+}
+
 func main() {
 	bc := NewBlockChain()
+	bc.AddBlock("我向MM转了50枚比特币")
+	bc.AddBlock("我向MM转了100枚比特币")
 	for index, block := range bc.blocks {
 		fmt.Printf("======================当前区块高度：%d  ======================\n", index+1)
 		fmt.Printf("前区块哈希值：%x\n", block.PrevHash)
