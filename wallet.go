5ebf2e9ebbe359e778401d7ce7686c9a3d3bb47e
hackerMao <hacker_murray@163.com>
2020-03-08 11:39:10 +0800

    modify GenerateWallet function

--- wallet.go
+++ wallet.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"BlockChain/base58"
-	"golang.org/x/crypto/ripemd160"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
+	"golang.org/x/crypto/ripemd160"
 	"log"
 )
 
@@ -31,15 +31,29 @@ func NewWallet() *Wallet {
 	// X、Y坐标拼接的字符串
 	publicKey := append(PublicKey.X.Bytes(), PublicKey.Y.Bytes()...)
 	return &Wallet{
-		PrivateKey:   PrivateKey,
-		PublicKey: publicKey,
+		PrivateKey: PrivateKey,
+		PublicKey:  publicKey,
 	}
 }
 
 // 生成地址
 func (self *Wallet) GenerateAddr() string {
+	rip160HashValue := publicKeyHash(self.PublicKey)
+	// 拼接version,总计21 bytes
+	version := byte(00)
+	payload := append([]byte{version}, rip160HashValue...)
+	// checksum
+	checkCode := CheckSum(payload)
+	// 25字节数据
+	payload = append(payload, checkCode...)
+	// base58编码，使用btcd包:go语言实现比特币全节点源码
+	address := base58.Encode(payload)
+	return address
+}
+
+func publicKeyHash(data []byte) []byte {
 	// 1、对公钥进行hash
-	pub_hash := sha256.Sum256(self.PublicKey)
+	pub_hash := sha256.Sum256(data)
 	// 新建一个ripmd160编码器
 	hash160 := ripemd160.New()
 	_, err := hash160.Write(pub_hash[:])
@@ -47,18 +61,13 @@ func (self *Wallet) GenerateAddr() string {
 		log.Panic(err)
 	}
 	rip160HashValue := hash160.Sum(nil)
-	// 拼接version,总计21 bytes
-	version := byte(00)
-	payload := append([]byte{version}, rip160HashValue...)
+	return rip160HashValue
+}
 
-	// checksum
+func CheckSum(payload []byte) []byte {
 	// 两次256hash
 	payloadHash := sha256.Sum256(payload)
 	rePayloadHash := sha256.Sum256(payloadHash[:])
 	checkCode := rePayloadHash[:4]
-	// 25字节数据
-	payload = append(payload, checkCode...)
-	// base58编码，使用btcd包:go语言实现比特币全节点源码
-	address := base58.Encode(payload)
-	return address
+	return checkCode
 }
